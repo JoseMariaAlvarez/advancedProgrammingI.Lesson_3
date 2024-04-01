6051d751cddd67357e90476649434a0995678bb3
jmalvarez <jmalvarez@uma.es>
2024-04-01 11:52:00 +0200

    First version.

---
+++ Circle.java
@@ -0,0 +1,28 @@
+public class Circle {
+
+	static final double PI = 3.141532d;
+	
+	private double radius;
+	
+public Circle(Point p, double r) {
+if (r < 0) {
+	throw new RuntimeException("Negative radius");
+	}
+// This line is not reached if r < 0
+if (p == null) {
+	throw new RuntimeException("Null center point");
+	}
+// This line is never reached if p == null
+radius = r; 	
+centre = p;
+}
+
+	public double calculatePerimeter() {
+		return 2.0d * PI * radius;
+	}
+	
+	public double calculateArea() {
+		return PI * Pi * radius;
+	}
+	
+}
